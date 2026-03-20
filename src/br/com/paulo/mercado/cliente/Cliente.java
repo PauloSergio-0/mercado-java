faae8ca30bc4b90dd8287a2cfee7a05501a429f1
Paulo Sérgio <paulosergiosrs08@gmail.com>
2026-03-20 12:30:20 -0300

    feat: added cliente

---
+++ src/br/com/paulo/mercado/cliente/Cliente.java
@@ -0,0 +1,19 @@
+package br.com.paulo.mercado.cliente;
+
+import javax.swing.*;
+
+public class Cliente {
+    private String nome;
+    private String cpf;
+    private String telefone;
+    private String endereco;
+
+    public Cliente(String nome , String cpf, String telefone, String endereco){
+        this.nome = nome ;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.endereco = endereco;
+    }
+
+
+}
